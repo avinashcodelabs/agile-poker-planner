b8ad8cb67bd6bfb7019207d4045a16720ac437d1
Khushbu Tameshwari <33195237+khushbuT24@users.noreply.github.com>
2023-09-25 12:54:55 +0530

    Responsiveness and style fix (#34)
    * room page responsiveness #23
    
    * fix: responsiveness fixes

--- src/components/deck.js
+++ src/components/deck.js
@@ -7,7 +7,7 @@ const Deck = ({ onVote, users, userName }) => {
 
   const fibonacciNumbers = generateFibonacciSeries();
   return (
-    <div className="join flex gap-4 p-2 flex-1 justify-center items-center">
+    <div className="join flex gap-4 p-2 flex-1 justify-center items-center flex-wrap">
       {fibonacciNumbers.map((number, index) => (
         <VoteCard onVote={onVote} number={number} key={index}></VoteCard>
       ))}
