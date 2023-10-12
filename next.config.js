f2b3d54b9613b0612a7b2656faa49daefabfeb03
avinash2wards <avinash2wards@outlook.com>
2023-10-12 23:06:46 +0530

    feat: pwa

--- next.config.js
+++ next.config.js
@@ -1,8 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
 
 const withBundleAnalyzer = require("@next/bundle-analyzer")({
   enabled: process.env.ANALYZE === "true",
 });
 
-module.exports = withBundleAnalyzer(nextConfig);
+const withPWA = require("next-pwa")({
+  dest: "src/public",
+});
+
+const nextConfig = {};
+
+module.exports = withBundleAnalyzer(withPWA(nextConfig));
--- src/app/layout.js
+++ src/app/layout.js
@@ -15,6 +15,27 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover"
+        />
+        <link rel="manifest" href="/manifest.json" />
+        <link
+          href="/icons/favicon-16x16.png"
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+        />
+        <link
+          href="/icons/favicon-32x32.png"
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+        />
+        <link rel="apple-touch-icon" href="/apple-icon.png"></link>
+        <meta name="theme-color" content="#317EFB" />
+      </head>
       <body className={roboto.className}>{children}</body>
     </html>
   );
