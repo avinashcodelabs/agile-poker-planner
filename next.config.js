1a62fb288e3bb0e7cf2800b50286d59e47304265
avinash2wards <avinash2wards@outlook.com>
2023-10-13 20:26:51 +0530

    fix: added millionjs

--- next.config.js
+++ next.config.js
@@ -1,8 +1,16 @@
+const million = require("million/compiler");
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+};
+
+const millionConfig = {
+  auto: { rsc: true },
+};
 
 const withBundleAnalyzer = require("@next/bundle-analyzer")({
   enabled: process.env.ANALYZE === "true",
 });
 
-module.exports = withBundleAnalyzer(nextConfig);
+module.exports = withBundleAnalyzer(million.next(nextConfig, millionConfig));
