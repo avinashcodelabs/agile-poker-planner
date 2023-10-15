ce9e553a40503b3ac451c67edc72d17a844d375d
avinashcodelabs <avinashcodelabs@gmail.com>
2023-10-15 16:15:52 +0530

    feat: dont reveal until we have a min 1 vote (#47)
    Co-authored-by: avinash2wards <avinash2wards@outlook.com>

--- src/components/drawer.js
+++ src/components/drawer.js
@@ -4,25 +4,25 @@ import { InviteColleague } from "./inviteColleague";
 
 export const Drawer = ({ room }) => {
   return (
-    <div class="drawer drawer-end w-9 p-2 lg:drawer-open lg:w-80">
-      <input id="my-drawer-2" type="checkbox" class="drawer-toggle" />
-      <div class="drawer-content flex flex-col pt-3">
-        <label for="my-drawer-2" class="drawer-button lg:hidden">
+    <div className="drawer drawer-end w-9 p-2 lg:drawer-open lg:w-80">
+      <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
+      <div className="drawer-content flex flex-col pt-3">
+        <label htmlFor="my-drawer-2" className="drawer-button lg:hidden">
           <Image
             src={MenuIcon}
             alt="MenuIcon"
-            class="cursor-pointer"
+            className="cursor-pointer"
             width={16}
           />
         </label>
       </div>
-      <div class="drawer-side mt-24 lg:mt-0 drawer-teal-400">
+      <div className="drawer-side mt-24 lg:mt-0 drawer-teal-400">
         <label
-          for="my-drawer-2"
+          htmlFor="my-drawer-2"
           aria-label="close sidebar"
-          class="drawer-overlay"
+          className="drawer-overlay"
         ></label>
-        <ul class="flex justify-start p-2 lg:p-0 w-80 min-h-full text-base-content bg-teal-50 lg:bg-transparent ">
+        <ul className="flex justify-start p-2 lg:p-0 w-80 min-h-full text-base-content bg-teal-50 lg:bg-transparent ">
           <li>
             <InviteColleague room={room}></InviteColleague>
           </li>
