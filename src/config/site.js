3c4e63528512215c82f86f64a81570a8af0c0e89
avinashcodelabs <avinashcodelabs@gmail.com>
2023-10-21 15:03:14 +0530

    feat: added social media preview and pwa meta info (#64)
    Co-authored-by: avinash2wards <avinash2wards@outlook.com>

---
+++ src/config/site.js
@@ -0,0 +1,8 @@
+const siteConfig = {
+  name: "Agile Poker Planner",
+  description:
+    "Collaboratively estimate and prioritize work stories with the Agile Poker Planner app.",
+  url: "https://www.agilepokerplanner.com",
+};
+
+export { siteConfig };
