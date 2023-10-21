d371528343392dafeff20c7f8db80177dfbf7679
avinash2wards <avinash2wards@outlook.com>
2023-10-21 23:42:04 +0530

    fix: image optimized for meta data

--- src/app/layout.js
+++ src/app/layout.js
@@ -50,13 +50,16 @@ export const metadata = {
     title: siteConfig.name,
     description: siteConfig.description,
     siteName: siteConfig.name,
-    images: [`${siteConfig.url}/opengraph-image.jpg`],
   },
   twitter: {
     card: "summary_large_image",
     title: siteConfig.name,
     description: siteConfig.description,
-    images: [`${siteConfig.url}/opengraph-image.jpg`],
+    images: [
+      {
+        ima,
+      },
+    ],
     creator: "@agilepokerplan",
   },
   icons: {
