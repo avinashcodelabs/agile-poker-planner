659406587cdc5aadf661a64aac230ee2311ba0ed
Khushbu <khushbukatie@gmail.com>
2023-11-05 18:23:01 +0530

    feat: microsoft clarity integration

--- src/app/layout.js
+++ src/app/layout.js
@@ -79,6 +79,15 @@ export default function RootLayout({ children }) {
           gtag('config', 'G-7Z1TDZ6CSW');
         `}
       </Script>
+      <Script id="clarity">
+        {`
+          (function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+          })(window, document, "clarity", "script", "jlada0vih0");
+        `}
+      </Script>
       <body className={roboto.className}>{children}</body>
     </html>
   );
